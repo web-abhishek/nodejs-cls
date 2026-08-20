991fbfb6561a6750529dd5ea85e9ae106a46f045
web-abhishek <web.abhiacharya@gmail.com>
2026-08-20 20:28:03 +0530

    get method

--- src/main.js
+++ src/main.js
@@ -1,18 +1,34 @@
 const express = require('express');
 
+// const mysql = require('mysql');
+
+// const con = mysql.createConnection({
+//     user: 'root',
+//     password: '',
+//     database: 'foodlist',
+//     host: '127.0.0.1'
+// })
+
+// con.connect((error)=>{
+//     if (error) throw error;
+//     else{
+//         console.log('MySQL Database is connected');
+//     }
+// })
+
 const mysql = require('mysql');
 
 const con = mysql.createConnection({
     user: 'root',
     password: '',
-    database: 'foodlist',
+    database: 'fooddb',
     host: '127.0.0.1'
 })
 
-con.connect((error)=>{
-    if (error) throw error;
-    else{
-        console.log('MySQL Database is connected');
+con.connect((error) => {
+    if (error) throw error
+    else {
+        console.log('MySQL Database is connected Successfully..!');
     }
 })
 
@@ -31,15 +47,38 @@ app.get('/', (req,res)=>{
     res.send (`<h1>Node js + MySQL</h1>`)
 })
 
-app.get('/api/foods', (req,res)=>{
-    var SQL = 'select * from foods';
-    con.query(SQL, (error, result)=>{
-        if (error) throw error;
-        else{
-            res.status(200).json({'info':result});
+app.get('/api/foods', (req, res) => {
+    var SQL = 'SELECT * FROM food';
+    con.query(SQL, (error, result) => {
+        if (error) throw error
+        else {
+            res.status(200).json({ 'info': result });
         }
     })
 })
+
+app.get('/api/food/:id', (req, res) => {
+    const id = req.params.id;
+
+    const SQL = 'SELECT * FROM food WHERE id = ?';
+
+    con.query(SQL, [id], (error, result) => {
+        if (error) {
+            return res.status(500).json({
+                message: 'Database error'
+            });
+        }
+
+        if (result.length === 0) {
+            return res.status(404).json({
+                message: 'Food not found'
+            });
+        }
+
+        res.status(200).json(result[0]);
+    });
+});
+
 app.listen(port,()=>{
     console.log(`Server is started at ${port}`);
 })
