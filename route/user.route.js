194735477ed589098ad340a1cf7be316ae57e741
web-abhishek <web.abhiacharya@gmail.com>
2026-08-26 15:59:52 +0530

    user-pass hashing(bcryptjs)

--- route/user.route.js
+++ route/user.route.js
@@ -1,9 +1,11 @@
+
 const express = require('express');
 const bcrypt = require('bcryptjs')
 const userRouter = express.Router();
 
 const con = require('../dbConnection/dbCon');
 
+// User signup :
 userRouter.post('/user/signup', (req, res) => {
     
     let name = req.body.name;
@@ -17,7 +19,7 @@ userRouter.post('/user/signup', (req, res) => {
     values('${name}',
             '${email}',
             '${phone}',
-            '${hashPass}' 
+            '${hashPass}'
             )`;
     
     con.query(SQL, (error, result) => {
@@ -26,7 +28,7 @@ userRouter.post('/user/signup', (req, res) => {
         }
         else {
             if (result.affectedRows == 1) {
-                res.status(200).json({'message': 'Successfull..!'})
+                res.status(200).json({'message': 'SignUp Successfully..!'})
             }
             else {
                 res.status(200).json({'message': 'Something went wrong..!'})
@@ -35,6 +37,37 @@ userRouter.post('/user/signup', (req, res) => {
     })
 })
 
+// User SignIn:
+userRouter.post('/user/signin', (req, res) => {
+    
+    let email = req.body.email;
+    let dbPass = ''
+    let pass = req.body.pass;
+
+    var SQL = `SELECT * FROM user WHERE email='${email}'`;
+    
+    con.query(SQL, (error, userData) => {
+        if (error) {
+            res.status(200).json({'message': error.sqlMessage})
+        } else {
+            if (!userData.length) {
+                res.status(200).json({'message': 'Email doesnot exist..!'})
+            } else {
+                userData.forEach((userInfo) => {
+                    dbPass = userInfo.pass;
+                    let passMatch = bcrypt.compareSync(pass, dbPass);
+
+                    if (passMatch) {
+                        res.status(200).json({'message': 'Successfully SignIn..!'})
+                    } else {
+                        res.status(200).json({'message': 'Password doesnot match..!'})
+                    }
+                })
+            }
+        }
+    })
+    
+})
 
 module.exports = userRouter;
-console.log('User Router ready to use..!')
+console.log('User Router ready to use..!');
